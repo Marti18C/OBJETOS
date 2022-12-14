516ea3697a65edee1f74d1ce1d3c9db7eb9077f1
Martina Carrasco <martinacarrascop@gmail.com>
2022-12-14 13:18:22 -0500

    EJERCICIO LISTO
    Ejercicio listo según el tutorial del profe

--- sketch.js
+++ sketch.js
@@ -1,19 +1,40 @@
-let nPelotas = 100;
+//let nPelotas = random();
 let pelotas = [];
+//let t = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < nPelotas; i++) {
-        pelotas[i] = new RandomWalek(i);
-    }
+    // for (let i = 0; i < nPelotas; i++) { pelotas[i] = new RandomWalek(i);}
 }
 
 function draw() {
+    // background(200);
 
-    for (let i = 0; i < nPelotas; i++) {
-        pelotas[i].update();
-        pelotas[i].display();
+
+    //for (let i = 0; i < nPelotas; i++) {
+    for (let i = 0; i < pelotas.length; i++) {
+        //pelotas[i].update(t);
+        if (pelotas[i].isAlive) {
+            pelotas[i].update();
+            pelotas[i].display();
+        } else {
+            pelotas.splice(i, 1);
+        }
     }
+    // t += 0.01;
+}
+
+function mouseClicked() {
+    background("rgba(255, 255, 255, .2)");
+    for (let i = 0; i < 200; i++) {
+        let nuevaPlanta = new Plantita(mouseX, mouseY);
+        pelotas.push(nuevaPlanta);
+    }
+
+
+    //let nuevaPlanta = new Plantita(mouseX, mouseY);
+    //pelotas.push(nuevaPlanta);
+    //print(pelotas.length);
 
 }
 
@@ -23,30 +44,54 @@ function draw() {
 //--------------Random Walker
 //---------------------------
 
-class RandomWalek {
-    constructor(_name) {
+class Plantita {
+    constructor(_mouseX, _mouseY) {
 
-        this.red = random(0, 50);
+        this.red = random(150, 255);
         this.green = random(100, 255);
         this.blue = random(100, 150);
 
-        this.name = _name;
-        this.posX = width / 2;
-        this.posY = height / 2;
-        this.speed = random(2, 10);
-        this.diametro = random(10, 50);
-        print('Hola, soy la pelota' + this.name);
+        this.t = 0;
+        this.tSpeed = random(0.2);
+        this.noiseShift = random(1000);
+        this.lifeSpam = int(random(30, 50));
+
+        this.isAlive = true;
+
+        this.pos = createVector(_mouseX, mouseY);
+        this.speed = createVector(random(-4, 4), random(-4, 4));
+        this.diametro = random(10, 30);
+        this.bolitaFinal = this.diametro / 1.5;
+        print('Hola: viviré ' + this.lifeSpam + ' frames.');
 
     }
-    update() {
-        this.posX += random(-this.speed, this.speed);
-        this.posY += random(-this.speed, this.speed);
+    update(_t) {
+
+        this.speed.rotate(map(noise(this.t + this.noiseShift), 0, 1, -0.1, 0.1));
+
+        this.pos.add(this.speed)
+        this.t += this.tSpeed;
+
+        this.lifeSpam--;
 
     }
     display() {
+        stroke("rgba(1, 1, 1, .2)");
+        strokeWeight(3);
         fill(this.red, this.green, this.blue)
-        ellipse(this.posX, this.posY, this.diametro, this.diametro);
+        ellipse(this.pos.x, this.pos.y, this.diametro, this.diametro);
+        if (this.lifeSpam <= 0) {
+            this.muriending();
+        }
 
     }
+    muriending() {
+        this.diametro -= 0.6;
+        if (this.diametro <= 0) {
+            this.isAlive = false;
+            print("me morí, porque mi vida es " + this.isAlive);
+            ellipse(this.pos.x, this.pos.y, this.bolitaFinal, this.bolitaFinal);
+        }
 
+    }
 }
